528469c0053b3c7031ff92d7dcacc73dedba1186
George Ng <gngcp@uw.edu>
2016-06-28 14:18:31 -0700

    Completed game tutorial minus touch controls

--- Practice2DRoguelike/Assets/Scripts/Player.cs
+++ Practice2DRoguelike/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MovingObject {
 
@@ -7,6 +8,15 @@ public class Player : MovingObject {
     public int pointsPerFood = 10;
     public int pointsPerSoda = 20;
     public float restartLevelDelay = 1f;
+    public Text foodText;
+
+    public AudioClip moveSound1;
+    public AudioClip moveSound2;
+    public AudioClip eatSound1;
+    public AudioClip eatSound2;
+    public AudioClip drinkSound1;
+    public AudioClip drinkSound2;
+    public AudioClip gameOverSound;
 
     private Animator animator;
     private int food;
@@ -17,6 +27,8 @@ public class Player : MovingObject {
         animator = GetComponent<Animator>();
 
         food = GameManager.instance.playerFoodPoints;
+
+        foodText.text = "Food : " + food;
 
         base.Start();
 	}
@@ -52,10 +64,15 @@ public class Player : MovingObject {
     protected override void AttemptMove<T> (int xDir, int yDir)
     {
         food--;
+        foodText.text = "Food : " + food;
 
         base.AttemptMove<T>(xDir, yDir);
 
         RaycastHit2D hit;
+        if (Move (xDir, yDir, out hit))
+        {
+            SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+        }
 
         CheckIfGameOver();
 
@@ -71,10 +88,14 @@ public class Player : MovingObject {
         } else if (other.tag == "Food")
         {
             food += pointsPerFood;
+            foodText.text = "+" + pointsPerFood + " Food : " + food;
+            SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
             other.gameObject.SetActive(false);
         } else if (other.tag == "Soda")
         {
             food += pointsPerSoda;
+            foodText.text = "+" + pointsPerSoda + " Food : " + food;
+            SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
             other.gameObject.SetActive(false);
         }
     }
@@ -95,6 +116,7 @@ public class Player : MovingObject {
     {
         animator.SetTrigger("playerHit");
         food -= loss;
+        foodText.text = "-" + loss + " Food : " + food;
         CheckIfGameOver();
     }
 
@@ -102,6 +124,8 @@ public class Player : MovingObject {
     {
         if (food <= 0)
         {
+            SoundManager.instance.PlaySingle(gameOverSound);
+            SoundManager.instance.musicSource.Stop();
             GameManager.instance.GameOver();
         }
     }
--- Practice2DRoguelike/Assets/Scripts/Wall.cs
+++ Practice2DRoguelike/Assets/Scripts/Wall.cs
@@ -5,6 +5,8 @@ public class Wall : MonoBehaviour {
 
     public Sprite dmgSprite;
     public int hp = 4;
+    public AudioClip chopSound1;
+    public AudioClip chopSound2;
 
     private SpriteRenderer spriteRenderer;
 
@@ -17,6 +19,7 @@ public class Wall : MonoBehaviour {
     // Update is called once per frame
     public void DamageWall(int loss)
     {
+        SoundManager.instance.RandomizeSfx(chopSound1, chopSound2);
         spriteRenderer.sprite = dmgSprite;
         hp -= loss;
         if (hp <= 0)
